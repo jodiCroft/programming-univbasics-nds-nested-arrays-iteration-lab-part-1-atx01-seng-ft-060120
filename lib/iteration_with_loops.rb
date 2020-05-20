ba8528f777b3ae0963897eb1f674e6df54cfdcba
Jodi Croft <jodipruett@gmail.com>
2020-05-20 15:50:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,12 @@
 def find_even_values(array_of_arrays)
-
-
+  row_index = 0
+  while row_index < array_of_arrays.length do
+    element_index = 0
+    while element_index < array_of_arrays[row_index].length do
+      if array_of_arrays[row_index][element_index].even?
+      p array_of_arrays[row_index][element_index]
+    else element_index += 1
+    end
+  end
+row_index += 1
 end
